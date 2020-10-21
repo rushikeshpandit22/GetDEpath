7e80f1f274355f2becbeb153f96397efe2a43094
Rushikesh pandit <rushikeshpandit22@gmail.com>
2020-10-21 16:50:37 +0530

    2nd commit

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 
 const aws = require('aws-sdk');
 const express = require('express');
+var jwt = require('jwt-simple'); 
+
 var port = process.env.PORT || 3000;
 const app = express();
 app.use(express.static('public'));
+app.use(require('body-parser').raw({
+   type: 'application/jwt'
+}));
 
 // Main, error and success views
 
 app.get('/', function (request, response) {
-  response.sendFile(__dirname + '/index.html');
+	var jwtToken = req.body.jwt;
+	console.log(jwtToken);
+	response.sendFile(__dirname + '/index.html');
 });
 app.get("/success", function (request, response) {
   response.sendFile(__dirname + '/success.html');
