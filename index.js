202471676bcb79178869a5d373aafe3167645502
Rushikesh Pandit <rushikeshpandit22@gmail.com>
2020-10-21 17:00:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(require('body-parser').raw({
 // Main, error and success views
 
 app.get('/', function (request, response) {
-	var jwtToken = req.body;
+	var jwtToken = request.body;
 	console.log(jwtToken);
 	response.sendFile(__dirname + '/index.html');
 });
