7ebd59d3071ba5ce67cf9528ae21aa3304c28605
Rushikesh pandit <rushikeshpandit22@gmail.com>
2020-10-22 12:53:45 +0530

    updated with the auth api

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express');
 var jwt = require('jwt-simple'); 
 const jwt = require('express-jwt');
 const jwksRsa = require('jwks-rsa');
+const axios = require('axios');
 
 var port = process.env.PORT || 3000;
 const app = express();
@@ -14,9 +15,34 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
 var clientid = process.env.Client_Id;
 var ClientSecret = process.env.Client_Secret;
-console.log(clientid);
+
+//Connect with the Marketing cloud using client id and client secret
+
+var conData = {
+	'clientId': process.env.CLIENT_ID,
+	'clientSecret': process.env.CLIENT_SECRET  
+}
+axios({
+	method:'post',
+	url:'https://auth.exacttargetapis.com/v1/requestToken',
+	data: conData,
+	headers:{
+	'Content-Type': 'application/json',
+	}
+})
+.then(function(response) {
+	console.log(response);
+	responsefromWeb.send('Authorization Sent');		
+	token = response.data.accessToken;				//Got the token
+
+}).catch(function (error) {
+	console.log(error);								//Authorization failed  	
+	responsefromWeb.send(error);
+});
+
 // Main, error and success views
 
 app.get('/', function (request, response) {
